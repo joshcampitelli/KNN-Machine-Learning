7b1006ce030f67d428b9510aae53de7d22170906
ethanroym <ethan.r.morrill@gmail.com>
2017-10-25 21:24:01 -0400

    correct issue #6

--- src/main.java
+++ src/main.java
@@ -7,19 +7,19 @@ public class main {
 
         MachineLearning Housing = new MachineLearning("housing");
 
-        Housing.Learn("h1",{new CartesianMetric(12, 25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(500000)});
+        Housing.Learn("h1",new CartesianMetric(12, 25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(500000));
         Housing.Learn("h2", new CartesianMetric(10, 50), new IntegerMetric(1000), new EnumMetric("old"), new IntegerMetric(300000));
-        Housing.Learn("h3",{new CartesianMetric(12, 25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(400000)});
+        Housing.Learn("h3",new CartesianMetric(12, 25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(400000));
 
         //Need to add print statement for representing the return values
-        Housing.Predict("h4", new CartesianMetric(15, 20), new IntegerMetric(1000), new EnumMetric("new"));
-        Housing.Predict("h5", new CartesianMetric(0,0), new IntegerMetric(1100), new EnumMetric("new"));
-        Housing.Predict("h6", new CartesianMetric(20, 40), new IntegerMetric(900), new EnumMetric("old"));
+        Housing.Predict(1,"h4", new CartesianMetric(15, 20), new IntegerMetric(1000), new EnumMetric("new"));
+        Housing.Predict(2,"h5", new CartesianMetric(0,0), new IntegerMetric(1100), new EnumMetric("new"));
+        Housing.Predict(1,"h6", new CartesianMetric(20, 40), new IntegerMetric(900), new EnumMetric("old"));
 
         //Need something here to show the total error, either a getter or print at end of PredictError
-        Housing.PredictError("h7", new CartesianMetric(15,25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(500000));
-        Housing.PredictError("h8", new CartesianMetric(20, 40), new IntegerMetric(1000), new EnumMetric("old"), new IntegerMetric(350000));
-        Housing.PredictError("h9", new CartesianMetric(10, 30), new IntegerMetric(1100), new EnumMetric("new"), new IntegerMetric(450000));
+        Housing.PredictError(1,"h7", new CartesianMetric(15,25), new IntegerMetric(1200), new EnumMetric("new"), new IntegerMetric(500000));
+        Housing.PredictError(2,"h8", new CartesianMetric(20, 40), new IntegerMetric(1000), new EnumMetric("old"), new IntegerMetric(350000));
+        Housing.PredictError(3,"h9", new CartesianMetric(10, 30), new IntegerMetric(1100), new EnumMetric("new"), new IntegerMetric(450000));
 
         MachineLearning Ladder = new MachineLearning("ladder");
 
@@ -28,14 +28,14 @@ public class main {
         Ladder.Learn("L3", new IntegerMetric(20), new EnumMetric("new"), new IntegerMetric(10), new IntegerMetric(120));
 
         //Need to add print statement for representing the return values
-        Ladder.Predict("L4", new IntegerMetric(12), new EnumMetric("old"), new IntegerMetric(5));
-        Ladder.Predict("L5", new IntegerMetric(13), new EnumMetric("new"), new IntegerMetric(5));
-        Ladder.Predict("L6", new IntegerMetric(18), new EnumMetric("old"), new IntegerMetric(10));
+        Ladder.Predict(1,"L4", new IntegerMetric(12), new EnumMetric("old"), new IntegerMetric(5));
+        Ladder.Predict(2,"L5", new IntegerMetric(13), new EnumMetric("new"), new IntegerMetric(5));
+        Ladder.Predict(3,"L6", new IntegerMetric(18), new EnumMetric("old"), new IntegerMetric(10));
 
         //Need something here to show the total error, either a getter or print at end of PredictError
-        Ladder.PredictError("L7", new IntegerMetric(10), new EnumMetric("new"), new IntegerMetric(10), new IntegerMetric(50));
-        Ladder.PredictError("L8", new IntegerMetric(15), new EnumMetric("new"), new IntegerMetric(7), new IntegerMetric(100));
-        Ladder.PredictError("L9", new IntegerMetric(18), new EnumMetric("new"), new IntegerMetric(9), new IntegerMetric(110));
+        Ladder.PredictError(1,"L7", new IntegerMetric(10), new EnumMetric("new"), new IntegerMetric(10), new IntegerMetric(50));
+        Ladder.PredictError(2,"L8", new IntegerMetric(15), new EnumMetric("new"), new IntegerMetric(7), new IntegerMetric(100));
+        Ladder.PredictError(3,"L9", new IntegerMetric(18), new EnumMetric("new"), new IntegerMetric(9), new IntegerMetric(110));
 
 
     }
