1ab7857832bb5afb726f58b8a872a5b114796ef5
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2017-11-08 12:31:02 -0500

    Merge Fixes for gui.

--- src/Controller/MainWindowController.java
+++ src/Controller/MainWindowController.java
@@ -10,12 +10,11 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 //import Views.EditWindow;
 import javax.swing.JOptionPane;
 
 public class MainWindowController implements ActionListener, ListSelectionListener {
-	private ArrayList<MachineLearning> container = new ArrayList<MachineLearning>();
+	private ArrayList<MachineLearning> machineLearningArray = new ArrayList<MachineLearning>();
 
 	public void valueChanged(ListSelectionEvent e) {
 		// TODO Auto-generated method stub
@@ -25,21 +24,21 @@ public class MainWindowController implements ActionListener, ListSelectionListen
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("Edit")){
 			//System.out.println("Hello World");
-			new AlternateWindow();	
+			new AlternateWindow(new MetricController(machineLearningArray.get(machineLearningArray.size())));
 		} else if(e.getActionCommand().equals("Add Problem")){
 			JOptionPane option = new JOptionPane();
 			@SuppressWarnings("static-access")
 			String problemName = option.showInputDialog(null,"Please enter the name of your problem:","Start New Problem",JOptionPane.QUESTION_MESSAGE);
-			container.add(new MachineLearning(problemName));
-			new AlternateWindow();
+			machineLearningArray.add(new MachineLearning(problemName));
+			new AlternateWindow(new MetricController(machineLearningArray.get(machineLearningArray.size()-1)));
 			
 		}
 	}
 	
 	public DefaultListModel<String> getProblems(){
 		DefaultListModel<String> tmp = new DefaultListModel<String>();
-		for(int i = 0; i < container.size(); i++){
-			tmp.addElement(container.get(i).getProblem());
+		for(int i = 0; i < machineLearningArray.size(); i++){
+			tmp.addElement(machineLearningArray.get(i).getProblem());
 		}
 		return tmp;
 	}
