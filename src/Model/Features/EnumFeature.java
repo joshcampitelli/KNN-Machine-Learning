e02469295fdd90dd0ca9a96ca3fe7778470e4a05
lucky-mac11 <luckyjaguar@gmail.com>
2017-11-24 14:04:03 -0500

    Set up GenericFeature to be a super class instead of an interface

--- src/Model/Features/EnumFeature.java
+++ src/Model/Features/EnumFeature.java
@@ -1,36 +1,14 @@
 package Model.Features;
 
-public class EnumFeature implements GenericFeature {
-
-	private String name;
-	private String value;
-
-
+public class EnumFeature extends GenericFeature {
+	
 	/* This constructor requires two string values the name of the feature, and the discrete value.
 	 * 
 	 * @author Logan MacGillivray, Ethan Morrill
 	 */
 	public EnumFeature(String name, String value){
-
-		this.name = name;
-		this.value = value;
+		super(name, value);
 	}
-
-	/* See GenericFeature.getValue() for full java doc
-	 * This function returns the enum of the metric for viewing
-	 * 
-	 * @author Logan MacGillivray
-	 */
-	public Object getValue(){
-		return value;
-	}
-
-	/* See GenericFeature.getName() for full java doc
-	 * This function returns the name of the feature for viewing
-	 *
-	 * @author Ethan Morrill
-	 */
-	public String getName() { return name; }
 
 	public String toString() {
 		return name + " (Discrete): Value = " + value;
--- src/Model/Features/IntegerFeature.java
+++ src/Model/Features/IntegerFeature.java
@@ -1,34 +1,15 @@
 package Model.Features;
 
-public class IntegerFeature implements GenericFeature {
-	private int value;
-	private String name;
-
+public class IntegerFeature extends GenericFeature {
+	
 	/* This constructor requires an integer.  The value becomes the only
 	 * property to this metric, as the metric is used for size/volume metrics.
 	 * 
 	 * @author Logan MacGillivray
 	 */
-	public IntegerFeature(String name, int val){
-		value = val;
-		this.name = name;
+	public IntegerFeature(String name, int value){
+		super(name, value);
 	}
-
-	/* See GenericFeature.getValue() for full java doc
-	 * This function returns a positive integer
-	 * 
-	 * @author Logan MacGillivray
-	 */
-	public Object getValue(){
-		return value;
-	}
-
-	/* See GenericFeature.getName() for full java doc
-	 * This function returns the name of the feature for viewing
-	 *
-	 * @author Ethan Morrill
-	 */
-	public String getName() { return name; }
 
 	public String toString() {
 		return name + " (Integer): Value = " + value;
