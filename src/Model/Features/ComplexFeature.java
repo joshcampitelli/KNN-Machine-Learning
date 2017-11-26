9bee378d35e3889d79965644f07c302361c88a99
lucky-mac11 <luckyjaguar@gmail.com>
2017-11-26 15:27:38 -0500

    Created ComplexFeature

---
+++ src/Model/Features/ComplexFeature.java
@@ -0,0 +1,41 @@
+package Model.Features;
+
+import java.util.ArrayList;
+
+public class ComplexFeature extends GenericFeature {
+
+	/** @author Logan
+	 *  Construct a ComplexFeature made of smaller GenericFeatures
+	 *  
+	 *  @param name of the feature (String), other features that make up this one (ArrayList)
+	 */
+	public ComplexFeature(String name, ArrayList<GenericFeature> feature) {
+		super(name, feature);
+	}
+
+	/** @author Logan
+	 *  This function appends another feature to the ComplexFeature
+	 *  
+	 *  @param feature you would like to add (GenericFeature)
+	 */
+	@SuppressWarnings("unchecked")
+	public void append(GenericFeature feature) {
+		((ArrayList<GenericFeature>)value).add(feature);
+	}
+
+	/** @author Logan
+	 *  This function writes out the ComplexFeature into a String
+	 *  
+	 *  @param none
+	 */
+	@SuppressWarnings("unchecked")
+	public String toString(){
+		String rtn = name + "has the following features in it:\n";
+		ArrayList<GenericFeature> features = (ArrayList<GenericFeature>)value;
+		for(int i = 0; i < features.size(); i++) {
+			rtn += features.get(i).toString();
+		}
+		return rtn;
+	}
+	
+}
